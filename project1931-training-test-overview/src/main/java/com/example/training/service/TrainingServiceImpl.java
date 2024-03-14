c6b332a1c226ec910646114927353d11eaed10d8
koichi.ikeda <koichiikeda.0111@gmail.com>
2024-03-14 21:52:39 +0900

    test用のapplication.propertiesと本番用のapplication.propertiesで異なるものが読み込まれていることの確認を追加

--- project1931-training-test-overview/src/main/java/com/example/training/service/TrainingServiceImpl.java
+++ project1931-training-test-overview/src/main/java/com/example/training/service/TrainingServiceImpl.java
@@ -1,17 +1,22 @@
 package com.example.training.service;
 
-import com.example.training.entity.Training;
-import com.example.training.repository.TrainingRepository;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import com.example.training.entity.Training;
+import com.example.training.repository.TrainingRepository;
 
 @Service
 @Transactional
 public class TrainingServiceImpl implements TrainingService {
 
     private final TrainingRepository trainingRepository;
+
+    @Value("${logging.level.org.springframework.jdbc.support.JdbcTransactionManager}")
+    private String logLevel;
 
     public TrainingServiceImpl(TrainingRepository trainingRepository) {
         this.trainingRepository = trainingRepository;
@@ -24,6 +29,8 @@ public class TrainingServiceImpl implements TrainingService {
 
     @Override
     public Training findById(String id) {
+        // プロパティの確認
+        System.out.println("logLevel: " + logLevel);
         return trainingRepository.selectById(id);
     }
 
--- project1931-training-test-overview/src/test/java/com/example/training/TrainingApplicationTests.java
+++ project1931-training-test-overview/src/test/java/com/example/training/TrainingApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.training;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.training.entity.Training;
@@ -13,10 +14,15 @@ class TrainingApplicationTests {
 	@Autowired
 	private TrainingService service;
 
+	@Value("${logging.level.org.springframework.jdbc.support.JdbcTransactionManager}")
+	private String logLevel;
+
 	@Test
 	public void test_findById() {
 		Training training = service.findById("t01");
 		Assertions.assertThat(training.getTitle()).isEqualTo("ビジネスマナー研修");
+		// プロパティの確認
+		System.out.println("logLevel: " + logLevel);
 	}
 
 }
